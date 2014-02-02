562c740dc4c8f662c7c783fe8c7a0c692fe3786d
avjgit <avjmail@gmail.com>
2014-02-02 15:52:42 +0200

    saving and recreating state

--- android/coursera_android/w2lab_activity/SourceFiles/Skeletons/ActivityLab/ActivityLab/src/course/labs/activitylab/ActivityOne.java
+++ android/coursera_android/w2lab_activity/SourceFiles/Skeletons/ActivityLab/ActivityLab/src/course/labs/activitylab/ActivityOne.java
@@ -70,6 +70,11 @@ public class ActivityOne extends Activity {
 			// TODO:
 			// Restore value of counters from saved state
 			// Only need 4 lines of code, one for every count variable
+			mCreate = savedInstanceState.getInt(CREATE_KEY);
+			mRestart = savedInstanceState.getInt(RESTART_KEY);
+			mResume = savedInstanceState.getInt(RESUME_KEY);
+			mStart = savedInstanceState.getInt(START_KEY);
+		
 
 		}
 
@@ -160,6 +165,12 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Save state information with a collection of key-value pairs
 		// 4 lines of code, one for every count variable
+		savedInstanceState.putInt(CREATE_KEY, mCreate);
+		savedInstanceState.putInt(RESTART_KEY, mRestart);
+		savedInstanceState.putInt(RESUME_KEY, mResume);
+		savedInstanceState.putInt(START_KEY, mStart);
+		
+		super.onSaveInstanceState(savedInstanceState);
 
 	}
 
